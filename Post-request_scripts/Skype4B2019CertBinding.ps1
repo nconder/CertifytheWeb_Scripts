92ed3c1a21fc1e9fb217bd06d432a849751cf278
nick <nconder@users.noreply.github.com>
2019-08-28 21:20:29 -0500

    Update Skype4B2019CertBinding.ps1

--- Post-request_scripts/Skype4B2019CertBinding.ps1
+++ Post-request_scripts/Skype4B2019CertBinding.ps1
@@ -1,2 +1,3 @@
 # Asign certs to Skype for Business server 2019 Default and External type
+Import-Module SkypeforBusiness
 Set-CSCertificate -Type Default,WebServicesExternal -Thumbprint $result.ManagedItem.CertificateThumbprintHash -Confirm:$false
