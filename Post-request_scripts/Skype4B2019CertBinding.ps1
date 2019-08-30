c87f7e3fa2cf10f9861a0b9d2a3fea6e52f9b5ed
nick <nconder@users.noreply.github.com>
2019-08-30 18:13:10 -0500

    update for split brain dns

--- Post-request_scripts/Skype4B2019CertBinding.ps1
+++ Post-request_scripts/Skype4B2019CertBinding.ps1
@@ -1,3 +1,8 @@
 # Assign certs to Skype for Business server 2019 Default and External type
 Import-Module SkypeforBusiness
-Set-CSCertificate -Type Default,WebServicesExternal -Thumbprint $result.ManagedItem.CertificateThumbprintHash -Confirm:$false
+
+# set cert to default and external
+#Set-CSCertificate -Type Default,WebServicesExternal -Thumbprint $result.ManagedItem.CertificateThumbprintHash -Confirm:$false
+
+# set cert to all sites good for split brain dns
+Set-CSCertificate -Type Default,WebServicesInternal,WebServicesExternal -Thumbprint $result.ManagedItem.CertificateThumbprintHash -Confirm:$false
